881e73e0e672da1300bf17bbd8df740ebfd60b10
lunata <nataly@krc.karelia.ru>
2014-12-29 15:26:47 +0300

    Some plans

---
+++ lib/algorithms/wsd_in_wikt/PWLesk.php
@@ -0,0 +1,20 @@
+<?php
+
+class PWLesk {
+
+    /** Gets number of common words in two meanings (definitions) identified by meaning_id1 and meaning_id2, 
+     * it will be counted only for meaninful POS [without stop-words].
+     * @return int number of common words in two definitions
+     */
+    static public function countIntersectionTwoMeanings(meaning_id1, meaning_id2) {
+    global $LINK_DB;
+        $rk = array();
+        
+        //TODO 
+
+        return $rk;
+    }
+
+    
+}
+?>
---
+++ lib/sql/long_lived/readme.txt
@@ -0,0 +1,3 @@
+This folder contains tables, which are long lived tables, 
+i.e. these tables will survive when new Wiktionary dump arrive.
+For example, the table 'll_stopwords' will be updated by data from new Wiktionary dump.
