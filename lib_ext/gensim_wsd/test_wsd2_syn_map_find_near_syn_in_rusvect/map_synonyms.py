4e11e01f78a89103982be81c5d8da367dfda16ea
componavt <andrew.krizhanovsky@gmail.com>
2017-06-01 17:29:51 +0300

    wsd syn map 2 (+ our fantasy about synonyms)

--- lib_ext/gensim_wsd/test_wsd2_syn_map_find_near_syn_in_rusvect/map_synonyms.py
+++ lib_ext/gensim_wsd/test_wsd2_syn_map_find_near_syn_in_rusvect/map_synonyms.py
@@ -39,48 +39,74 @@ words = lib.filter_vocab_words.filterVocabWords( sentence_words, model.vocab )
 print "Words in RusVectores: " + u', '.join(words)
 
 
-target_word = words[0]
-print "target word: " + target_word
+for target_word in words:
 
-# sentence without target word
-sentence_minus_target = words[:]
-sentence_minus_target.remove( target_word )
-print "Words in sentence without target word: " + u', '.join(sentence_minus_target)
+    # target_word = words[4]
+    print "target word: " + target_word
 
-sys.exit("\nLet's stop and think.")
-
-
-
-# calculate sentence average vector
-arr_vectors = []
-for w in words:
-    # print u"    - '{}'".format( model[ w ] )
-    arr_vectors.append( model[ w ] )
-sentence_average_vector = np.average( arr_vectors, axis=0)
-#print "sentence average_vector: {}".format( sentence_average_vector )
+    # sentence without target word
+    sentence_minus_target = words[:]
+    sentence_minus_target.remove( target_word )
+    print "Words in sentence without target word: " + u', '.join(sentence_minus_target)
 
 
-# replace one word in sentence by synonym (replace w_remove by w_add)
-from synonyms_data import word_syn
 
-w_remove = u'плотный'
-w_add_array = word_syn [w_remove] # load synonyms to the word w_remove from the file synonyms_data.py
 
-for w in word_syn:
-    w_remove = w                # replace word
-    w_add_array = word_syn[w]   # by word's synonyms
+    # calculate sentence average vector
+    arr_vectors = []
+    for w in sentence_minus_target:
+        # print u"    - '{}'".format( model[ w ] )
+        arr_vectors.append( model[ w ] )
+    sentence_average_vector = np.average( arr_vectors, axis=0)
+    #print "sentence average_vector: {}".format( sentence_average_vector )
 
-    # if the word is absent in the sentence, then we cannot replace this word, skip this iteration
-    if w_remove not in words:
-        continue
 
-    for w_add in w_add_array:
-        if w_add not in model.vocab:
-            print "Error: " + w_add + " not in RusVectores dictionary!"
+    # copy words from sentence_minus_target to words_with_syn
+    words_with_syn = sentence_minus_target[:]
 
-        words_with_syn = [w_add if w == w_remove else w for w in words]
 
-        print "\nWords in RusVectores dict   : " + ', '.join(words)
-        print "One synonym changed in list : " + ', '.join(words_with_syn)
-        sim = model.n_similarity(words, words_with_syn)
-        print "sim = {:5.3f}".format(sim)
+    # replace one word in sentence by synonym (replace w_remove by w_add)
+    from synonyms_data import word_syn
+
+    w_remove = u'ветер'
+    w_add_array = word_syn [w_remove] # load synonyms to the word w_remove from the file synonyms_data.py
+
+    for w in word_syn:
+        w_remove = w                # replace word
+        w_add_array = word_syn[w]   # by word's synonyms
+
+        # if the word is absent in the sentence, then we cannot replace this word, skip this iteration
+        if w_remove not in words:
+            continue
+
+        for w_add in w_add_array:
+            if w_add not in model.vocab:
+                print "Error: " + w_add + " not in RusVectores dictionary!"
+                continue
+
+            words_with_syn_new = [w_add if w == w_remove else w for w in words_with_syn]
+            words_with_syn = words_with_syn_new[:] # copy words from words_with_syn_new to words_with_syn
+
+            # print "\nWords in RusVectores dict   : " + ', '.join(words)
+            print "One synonym changed in list : " + ', '.join(words_with_syn)
+
+            # calculate average vector of sentence with synonym without target word
+            arr_vectors = []
+            for w in words_with_syn:
+                # print u"    - '{}'".format( model[ w ] )
+                arr_vectors.append( model[ w ] )
+            sentence_aver_vect_with_syn_wotarget = np.average( arr_vectors, axis=0)
+
+            vect_target_syn = model[ target_word ] - sentence_average_vector + sentence_aver_vect_with_syn_wotarget
+            # print "vect_target_syn: {}".format( vect_target_syn )
+
+            arr_target_synonyms = model.similar_by_vector(vect_target_syn, topn=1, restrict_vocab=None)
+    #        print "Calculated target synonyms: " + u', '.join(arr_target_synonyms)
+            print u'{0} {1}'.format(arr_target_synonyms[0][0], arr_target_synonyms[0][1])
+
+            sys.exit("\nLet's stop and think.")
+
+
+
+
+
